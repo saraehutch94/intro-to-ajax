2716a0e21c75c51eca511595d66dbda3c690d112
Sara Hutchinson <sarahutchinson@Saras-MacBook-Pro-2.local>
2021-10-23 12:39:19 -0400

    remove console.logs

--- script.js
+++ script.js
@@ -25,13 +25,11 @@ function handleSubmit(evt) {
     evt.preventDefault(); // this turns off the default page refresh behavior in the browser
     // grab the movie title we're submitting in the form
     const movieTitle = $input.val();
-    console.log(movieTitle);
     // use that data to request a specific movie
+    
     // Using jQuery's $.ajax() method
     // We use it to request information from a resource using AJAX
     // This method when called returns a JavaScript promise object
-
-    // const promise = $.ajax("https://www.omdbapi.com/?apikey=18275115&t=joker");
 
     // inspecting our JS Promise
     // console.log(promise);
@@ -42,8 +40,6 @@ function handleSubmit(evt) {
     $.ajax(`${BASE_URL}?apikey=${API_KEY}&t=${movieTitle}`)
     .then(function(data) {
         // success callback function
-        // console.log("promise fulfilled!");
-        // console.log(data);
         $title.text(data.Title);
         $year.text(data.Year);
         $rated.text(data.Rated);
