4b96e5247a6cf319715b33ed05baf4ff311150c9
Sara Hutchinson <sarahutchinson@Saras-MacBook-Pro-2.local>
2021-10-23 11:37:42 -0400

    add then method to promise

--- script.js
+++ script.js
@@ -21,8 +21,21 @@
 // We use it to request information from a resource using AJAX
 // This method when called returns a JavaScript promise object
 
-const promise = $.ajax("https://www.omdbapi.com/?apikey=18275115&t=joker");
+// const promise = $.ajax("https://www.omdbapi.com/?apikey=18275115&t=joker");
 
 // inspecting our JS Promise
-console.log(promise);
+// console.log(promise);
 
+// the .then() method is how we assign intructions to
+// the promise object so we can tell it what to do
+// when the promise is fulfilled
+$.ajax("https://www.omdbapi.com/?apikey=18275115&t=joker")
+.then(function(data) {
+    // success callback function
+    console.log("promise fulfilled!");
+    console.log(data);
+}, function(error) {
+    // failure callback function
+    console.log("promise failed");
+    console.log(error);
+});
