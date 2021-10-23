5adeae4a77d6d8f88a1c95448a63d6d375b65664
Sara Hutchinson <sarahutchinson@Saras-MacBook-Pro-2.local>
2021-10-23 11:19:53 -0400

    added AJAX method and JS promise

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-alert("hello!");
+// Constants - Data that does not change
+
+
+
+// Variables - Data that changes
+
+
+
+// Cached elements - DOM elements
+
+
+
+// Event listeners
+
+
+
+// Functions
+
+
+// Using jQuery's $.ajax() method
+// We use it to request information from a resource using AJAX
+// This method when called returns a JavaScript promise object
+
+const promise = $.ajax("https://www.omdbapi.com/?apikey=18275115&t=joker");
+
+// inspecting our JS Promise
+console.log(promise);
+
